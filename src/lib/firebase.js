84b880de369affb6937605de625f113e40d5565d
unknown <jagdeeshbhavani22@gmail.com>
2024-06-14 00:23:45 +0530

    feat:register and login completed 2

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 const firebaseConfig = {
-  apiKey: "AIzaSyBhocxrMoaegPqaT6n-rEVDLiO9WlpyMy0",
-  authDomain: "reactchat-8aac9.firebaseapp.com",
-  projectId: "reactchat-8aac9",
-  storageBucket: "reactchat-8aac9.appspot.com",
-  messagingSenderId: "513508638944",
-  appId: "1:513508638944:web:843dd69b992c5d20b59085",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 // Initialize Firebase
